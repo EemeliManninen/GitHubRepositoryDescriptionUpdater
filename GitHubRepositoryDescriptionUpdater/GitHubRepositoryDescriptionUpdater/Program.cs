95a185eb9040bd81704fd8356362d84532030c6e
Eemeli Manninen <mannineneemeli@gmail.com>
2022-11-15 00:26:12 +0200

    Logic for updating repository description

--- GitHubRepositoryDescriptionUpdater/GitHubRepositoryDescriptionUpdater/Program.cs
+++ GitHubRepositoryDescriptionUpdater/GitHubRepositoryDescriptionUpdater/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿var client = new HttpClient();
+
+var token = "";
+
+var description = $"This description was updated by a bot at {DateTime.Now.ToString("F")}";
+
+using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, "https://api.github.com/repos/EemeliManninen/GitHubRepositoryDescriptionUpdater"))
+{
+    requestMessage.Headers.Add("Accept", "application/json");
+    requestMessage.Headers.Add("User-Agent", "Other");
+    requestMessage.Headers.Add("Authorization", "Bearer " + token);
+    requestMessage.Content = new StringContent("{\"description\": \"" + description + "\"}");
+
+    var result = client.SendAsync(requestMessage).Result;
+}
